97f0f4c424d1fc7882381182e59da827a0286522
Quentin MACHARD <quentin.machard@niji.fr>
2017-10-24 16:12:13 +0200

    Feat. Create Pagination

--- src/containers/TravelsContainer.js
+++ src/containers/TravelsContainer.js
@@ -3,7 +3,9 @@ import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 
 import TravelsListComponent from "../components/TravelsListComponent";
+import PaginationComponent from "../components/PaginationComponent";
 import LoaderComponent from '../components/LoaderComponent';
+
 import * as travelsActions from '../actions/TravelsActions';
 
 class TravelsContainer extends Component {
@@ -22,17 +24,29 @@ class TravelsContainer extends Component {
     render () {
         const {travels, isLoading} = this.props;
 
+        let currentPage = travels['@id'] ? parseInt(travels['@id'].replace('/app_dev.php/travels?page=', ''), 0) : null;
+
         return (
             <div>
                 {
                     isLoading ? (
                         <LoaderComponent />
                     ) : (
-                        <TravelsListComponent travels={travels['hydra:member']} />
+                        <div>
+                            <TravelsListComponent travels={travels['hydra:member']} />
+                            <PaginationComponent
+                                totalItems={travels['hydra:totalItems']}
+                                itemsPerPage={travels['hydra:itemsPerPage']}
+                                currentPage={currentPage}
+                                onPageChange={(page) => {
+                                    this.fetchTravels(page);
+                                }}
+                            />
+                        </div>
                     )
                 }
 
-                <div className="btn-group" role="group" aria-label="...">
+                <div className="btn-group" role="group">
                     <button onClick={this.addTravel} className="btn btn-default btn-sm"><i className="glyphicon glyphicon-plus"/></button>
                 </div>
             </div>
