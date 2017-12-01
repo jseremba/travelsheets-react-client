e2408c038d69cac9337e89a174af4951ef8bf437
Quentin Machard <quentin.machard@gmail.com>
2017-12-01 15:03:22 +0100

    Feat. Submit and validate form to add Travel

--- src/actions/TravelAddActions.js
+++ src/actions/TravelAddActions.js
@@ -1,11 +1,11 @@
 import * as TravelAddConstants from '../constants/TravelAddConstants';
 
-export const save = () => {
+export const save = (values) => {
     return dispatch => {
         dispatch({
             type: TravelAddConstants.SAVE_REQUESTED
         })
-    }
+    };
 };
 
 /**
@@ -32,4 +32,42 @@ export const closeModal = () => {
             type: TravelAddConstants.CLOSE_MODAL
         });
     };
+};
+
+/**
+ * Action to update value of form item
+ *
+ * @param name
+ * @param value
+ *
+ * @returns {function(*)}
+ */
+export const updateValue = (name, value) => {
+    return dispatch => {
+        dispatch({
+            type: TravelAddConstants.UPDATE_VALUE,
+            name,
+            value
+        });
+    }
+};
+
+/**
+ * Action to set error of form item
+ *
+ * @param name
+ * @param error
+ * @param validation
+ *
+ * @returns {function(*)}
+ */
+export const setError = (name, error, validation = 'error') => {
+    return dispatch => {
+        dispatch({
+            type: TravelAddConstants.SET_ERROR,
+            name,
+            error,
+            validation
+        });
+    }
 };
--- src/reducers/TravelAddReducer.js
+++ src/reducers/TravelAddReducer.js
@@ -2,7 +2,29 @@ import * as TravelAddConstants from '../constants/TravelAddConstants';
 
 const initialState = {
     showModal: false,
-    travel: null
+    isLoading: false,
+    values: {
+        name: {
+            value: '',
+            error: null,
+            validation: null,
+        },
+        summary: {
+            value: '',
+            error: null,
+            validation: null,
+        },
+        dateStart: {
+            value: '',
+            error: '',
+            validation: null,
+        },
+        dateEnd: {
+            value: '',
+            error: null,
+            validation: null,
+        },
+    }
 };
 
 export default (state = initialState, action) => {
@@ -17,6 +39,39 @@ export default (state = initialState, action) => {
             return {
                 ...state,
                 showModal: false,
+            };
+
+        case TravelAddConstants.UPDATE_VALUE:
+            return {
+                ...state,
+                values: {
+                    ...state.values,
+                    [action.name]: {
+                        ...state.values[action.name],
+                        value: action.value,
+                        error: null,
+                        validation: null,
+                    }
+                }
+            };
+
+        case TravelAddConstants.SET_ERROR:
+            return {
+                ...state,
+                values: {
+                    ...state.values,
+                    [action.name]: {
+                        ...state.values[action.name],
+                        error: action.error,
+                        validation: action.validation,
+                    }
+                }
+            };
+
+        case TravelAddConstants.SAVE_REQUESTED:
+            return {
+                ...state,
+                isLoading: true,
             };
 
         default:
