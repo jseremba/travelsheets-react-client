d8f643e7ee95a8e3f427886aa5ca653d4603f93d
Quentin Machard <quentin.machard@gmail.com>
2017-12-01 13:17:39 +0100

    Feat. Create Constants used to add a Travel

---
+++ src/constants/TravelAddConstants.js
@@ -0,0 +1,6 @@
+export const OPEN_MODAL = 'travelAdd/OPEN_MODAL';
+export const CLOSE_MODAL = 'travelAdd/CLOSE_MODAL';
+
+export const SAVE_REQUESTED = 'travelAdd/SAVE_REQUESTED';
+export const SAVE_SUCCESS = 'travelAdd/SAVE_SUCCESS';
+export const SAVE_FAILURE = 'travelAdd/SAVE_FAILURE';
