b97ce95e972d8dd57ece9d504e00b9fbbdfadce3
Quentin Machard <quentin.machard@gmail.com>
2017-12-01 13:59:55 +0100

    Feat. Create the Reducer used to add a Travel

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import {routerReducer} from 'react-router-redux';
 import TravelsReducer from './TravelsReducer';
 import TravelReducer from './TravelReducer';
 import StepReducer from './StepReducer';
+import TravelAddReducer from "./TravelAddReducer";
 
 export default combineReducers({
     routing: routerReducer,
     travels: TravelsReducer,
     TravelReducer: TravelReducer,
-    StepReducer: StepReducer
+    TravelAddReducer: TravelAddReducer,
+    StepReducer: StepReducer,
 });
