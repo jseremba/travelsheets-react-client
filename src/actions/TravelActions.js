2d3aa1ad7bc9b19e8b7b0ba79cb2fcc5066c5f2d
Quentin Machard <quentin.machard@gmail.com>
2017-12-02 16:20:43 +0100

    Manage 404 error

--- src/actions/TravelActions.js
+++ src/actions/TravelActions.js
@@ -4,6 +4,7 @@ import Notifications from "react-notification-system-redux";
 import * as TravelConstants from '../constants/TravelConstants';
 
 import {API_URL} from "../settings/configuration";
+import {push} from "react-router-redux";
 
 export const fetchTravel = (id) => {
     return dispatch => {
@@ -20,22 +21,40 @@ export const fetchTravel = (id) => {
                     travel: response.data
                 });
             })
-            .catch(error => {
+            .catch((error, response) => {
                 dispatch({
                     type: TravelConstants.GET_FAILURE,
                     error: error
                 });
 
+                let message,
+                    action;
+
+                switch(error.response.status) {
+                    case 404:
+                        message = 'Ce voyage est introuvable';
+                        action = {
+                            label: 'Revenir à la liste',
+                            callback: () => {
+                                dispatch(push('/'));
+                            }
+                        };
+                        break;
+                    default:
+                        message = 'Une erreur s\'est produite lors de l\'accès au serveur.';
+                        action = {
+                            label: 'Réessayer',
+                            callback: () => {
+                                fetchTravel(id)(dispatch);
+                            }
+                        };
+                }
+
                 dispatch(Notifications.error({
                     title: 'Oh!',
-                    message: 'Une erreur s\'est produite lors de l\'accès au serveur.',
+                    message: message,
                     autoDismiss: 0,
-                    action: {
-                        label: 'Réessayer',
-                        callback: () => {
-                            fetchTravel(id)(dispatch);
-                        }
-                    }
+                    action: action
                 }));
             })
         ;
