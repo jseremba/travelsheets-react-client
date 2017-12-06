9a66cc6360c7a1c2ff3a58a2d9aa8df3507d4fdd
Quentin Machard <quentin.machard@gmail.com>
2017-12-06 15:45:16 +0100

    Feat. Create Action and Constant to delete an Attachment

--- src/constants/AttachmentsConstants.js
+++ src/constants/AttachmentsConstants.js
@@ -2,4 +2,8 @@ export const LIST_REQUESTED = 'AttachmentsConstants/LIST_REQUESTED';
 export const LIST_SUCCESS = 'AttachmentsConstants/LIST_SUCCESS';
 export const LIST_FAILURE = 'AttachmentsConstants/LIST_FAILURE';
 
+export const DELETE_REQUESTED = 'AttachmentsConstants/DELETE_REQUESTED';
+export const DELETE_SUCCESS = 'AttachmentsConstants/DELETE_SUCCESS';
+export const DELETE_FAILURE = 'AttachmentsConstants/DELETE_FAILURE';
+
 export const RESET_ATTACHMENTS = 'AttachmentsConstants/RESET_ATTACHMENTS';
