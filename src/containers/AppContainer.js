686c865b8a63a8a0fe437c1628b741129df0882c
Quentin Machard <quentin.machard@gmail.com>
2017-12-14 18:00:54 +0100

    Feat. Create the registration page

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -8,6 +8,7 @@ import TravelContainer from './TravelContainer';
 import NotificationsContainer from "./NotificationsContainer";
 import ConfirmContainer from "./ConfirmContainer";
 import LoginContainer from "./LoginContainer";
+import RegisterContainer from "./RegisterContainer";
 import NavbarContainer from "./NavbarContainer";
 
 const App = () => (
@@ -18,6 +19,7 @@ const App = () => (
         <PrivateRoute exact path="/travels/:id" component={TravelContainer}/>
 
         <Route exact path="/login" component={LoginContainer}/>
+        <Route exact path="/register" component={RegisterContainer}/>
 
         <NotificationsContainer/>
         <ConfirmContainer/>
